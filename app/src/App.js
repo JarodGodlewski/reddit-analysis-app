4561653a0df96a5301365ad9e7657a9e62ebb676
Jarod Godlewski <jarodgodlewski@live.com>
2020-11-15 21:16:00 -0500

    Refactoring from Routing to Scrolling
    Transitioning navigation from routing to scrolling, also changed projects file structure and styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
  
-import Home from './components/Home';
-import WordGraphs from './components/WordGraphs';
-import PerformanceGraphs from './components/PerformanceGraphs';
-import Error from './components/Error';
-import Navigation from './components/Navigation';
+import WordGraphs from './components/Graphs/WordGraphs';
+import PerformanceGraphs from './components/Graphs/PerformanceGraphs';
 import styled from 'styled-components'
+import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
  
 const App = () => {
     return (      
        <BrowserRouter>
-        <LayoutWrapper>
-          <Navigation />
-            <Switch>
-             <Route path="/" component={Home} exact/>
-             <Route path="/graphs/word-correlation" component={WordGraphs}/>
-             <Route path="/graphs/performance" component={PerformanceGraphs}/>
-            <Route component={Error}/>
-           </Switch>
-        </LayoutWrapper> 
+        <GlobalStyle />
+        <Hero />
+        <PerformanceGraphs />
+        <WordGraphs />
       </BrowserRouter>
     );
   }
 
-  const LayoutWrapper = styled.div`
-     background: #414073;
-  `;
  
 export default App;
